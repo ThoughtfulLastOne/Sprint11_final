649b7a83289d7c515c491551241427be2db5b3dd
ThoughtfulLastOne <117281417+ThoughtfulLastOne@users.noreply.github.com>
2023-05-20 12:49:48 +0300

    Update Dexterity.py

--- Dexterity.py
+++ Dexterity.py
@@ -1,15 +1,16 @@
-# 87049743
+# 87513007
 from typing import List, Tuple
 
 
 def get_max_points(matrix: List[str], k: int) -> int:
     points = 0
     count = [0] * 16
+    K = k * 2
     for number in matrix:
         if number != '.':
             count[int(number)] += 1
     for number in count:
-        if 0 < number <= k * 2:
+        if 0 < number <= K:
             points += 1
     return points
 
